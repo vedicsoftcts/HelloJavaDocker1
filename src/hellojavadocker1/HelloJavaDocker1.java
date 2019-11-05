c7cf187c782ff2c9e4673db2f40cb93a23941435
latif, vedic <vedicsoftcts@gmail.com>
2019-11-05 22:23:50 +0530

    1st commit HelloJavaDocker1

---
+++ src/hellojavadocker1/HelloJavaDocker1.java
@@ -0,0 +1,12 @@
+
+package hellojavadocker1;
+
+public class HelloJavaDocker1 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Java Docker");
+    }
+    
+}
